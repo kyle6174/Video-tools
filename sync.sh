b610935cbb18cae3a571d25be8011e65bb29bd93
kyle6174 <kbende@med.upenn.edu>
2012-07-12 12:41:35 -0400

    Trying to use softlinks to point to files. Not sure if this will work with git.

---
+++ sync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp /usr/local/bin/convert_video_to_streaming.pl /home/kbende/Video-tools/
+cp /usr/local/bin/convert_video_to_master.pl /home/kbende/Video-tools/
+cp /usr/local/bin/convert_video.pl /home/kbende/Video-tools/
+
+
+exit
