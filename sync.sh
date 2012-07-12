35bbc28f74ffd1b502f533430ac665d30288bff3
kyle6174 <kbende@med.upenn.edu>
2012-07-12 12:44:25 -0400

    Trying softlinks

--- sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cp /usr/local/bin/convert_video_to_streaming.pl /home/kbende/Video-tools/
-cp /usr/local/bin/convert_video_to_master.pl /home/kbende/Video-tools/
-cp /usr/local/bin/convert_video.pl /home/kbende/Video-tools/
-
-
-exit
